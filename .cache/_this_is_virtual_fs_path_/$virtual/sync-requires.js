7a7e0bfba3ad7fabf4a956b97df52f91f4228737
thomaskrug96 <thomaskrug1996@gmail.com>
2022-04-11 13:40:54 -0400

    commit

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,16 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/tkrug/Documents/GitHub/tkrug_portfolio/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/tkrug/Documents/GitHub/tkrug_portfolio/src/pages/404.js")),
+  "component---src-pages-archive-js": preferDefault(require("/Users/tkrug/Documents/GitHub/tkrug_portfolio/src/pages/archive.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/tkrug/Documents/GitHub/tkrug_portfolio/src/pages/index.js")),
+  "component---src-pages-pensieve-index-js": preferDefault(require("/Users/tkrug/Documents/GitHub/tkrug_portfolio/src/pages/pensieve/index.js")),
+  "component---src-pages-pensieve-tags-js": preferDefault(require("/Users/tkrug/Documents/GitHub/tkrug_portfolio/src/pages/pensieve/tags.js")),
+  "component---src-templates-post-js": preferDefault(require("/Users/tkrug/Documents/GitHub/tkrug_portfolio/src/templates/post.js")),
+  "component---src-templates-tag-js": preferDefault(require("/Users/tkrug/Documents/GitHub/tkrug_portfolio/src/templates/tag.js"))
+}
+
